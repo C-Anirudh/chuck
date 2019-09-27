911d669bf8ff2bfb92f7ca84fe8216da9ed47f62
C-Anirudh <canirudh16@gmail.com>
2019-09-27 19:28:25 +0530

    add basic routing and frontend

---
+++ ui/app.js
@@ -0,0 +1,24 @@
+var app = angular.module('chuck', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: 'html_components/home.html'
+        })
+        .when("/contact", {
+            templateUrl: 'html_components/contact.html'
+        })
+        .when("/about", {
+            templateUrl: 'html_components/about.html'
+        })
+        .otherwise({
+            templateUrl: 'html_components/error404.html'
+        })
+});
+
+$(document).ready(function() {
+    $.each($('#navbar').find('li'), function() {
+        $(this).toggleClass('active', 
+            window.location.pathname.indexOf($(this).find('a').attr('href')) > -1);
+    }); 
+});
